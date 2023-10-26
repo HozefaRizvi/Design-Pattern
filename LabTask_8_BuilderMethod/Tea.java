04c852875a593618306dd75747984b779c9c5667
Hozefa Hassan Rizvi <102030990+HozefaRizvi@users.noreply.github.com>
2023-10-26 10:21:11 +0500

    Adding BuilderMethod

---
+++ LabTask_8_BuilderMethod/Tea.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BuilderMethod;
+
+/**
+ *
+ * @author fa20-bse-019
+ */
+public abstract class Tea extends Beverage {
+    public Tea() {
+        super("Tea");
+        this.setprice(1.5f);
+    }
+}
